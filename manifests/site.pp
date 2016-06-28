b41cb9f0aee620453f94f6c51af94a6c0e9e5caf
ginginhub <virginia.galvin@noaa.gov>
2016-06-28 14:23:28 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,4 +45,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
-}
+  }
+  
+  file { '/etc/motd':
+  ensure =>file,
+  owner -> 'root',
+  content => "Happy Birthday",
+  }
+  
+  
+  
