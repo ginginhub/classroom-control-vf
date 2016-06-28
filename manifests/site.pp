6972c54811d51331a9581cb9da02ae56cc035ece
ginginhub <virginia.galvin@noaa.gov>
2016-06-28 15:08:42 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -51,7 +51,7 @@ node default {
   #owner => 'root',
   #content => "Happy Birthday\n",
   #}
-  exec {"cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+  exec {"cowsay 'Welcome to ${::fqdn}!'>/etc/motd":
   path => '/usr/bin:/usr/local/bin',
   creates => '/etc/motd',
   }
