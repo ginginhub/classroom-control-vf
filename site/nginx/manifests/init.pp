56623864d2ed0525efed0ce29923f72b9fff41a9
ginginhub <virginia.galvin@noaa.gov>
2016-06-29 06:56:20 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,12 +2,14 @@ class nginx {
 package { 'nginx':
 ensure => present,
 }
+
 file { '/var/www':
 ensure => directory,
 owner => 'root',
 group => 'root',
 mode => '0775',
 }
+
 file { '/var/www/index.html':
 ensure => file,
 owner => 'root',
@@ -15,6 +17,7 @@ group => 'root',
 mode => '0664',
 source => 'puppet:///modules/nginx/index.html',
 }
+
 file { '/etc/nginx/nginx.conf':
 ensure => file,
 owner => 'root',
@@ -24,12 +27,14 @@ source => 'puppet:///modules/nginx/nginx.conf',
 require => Package['nginx'],
 notify => Service['nginx'],
 }
+
 file { '/etc/nginx/conf.d':
 ensure => directory,
 owner => 'root',
 group => 'root',
 mode => '0775',
 }
+
 file { '/etc/nginx/conf.d/default.conf':
 ensure => file,
 owner => 'root',
@@ -39,9 +44,10 @@ source => 'puppet:///modules/nginx/default.conf',
 require => Package['nginx'],
 notify => Service['nginx'],
 }
+
 service { 'nginx':
 ensure => running,
 enable => true,
-
+}
 
 }
